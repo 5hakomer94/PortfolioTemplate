f632b323f253d1db0a036853132de83048384d94
shakib <shakshep@gmail.com>
2018-05-29 23:33:39 +0430

    first commit

---
+++ Bootstrap1/Scripts/custom.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    $(function () {
+        $(window).on('scroll', function () {
+            var $nav = $('.navbar.navbar-collapse'),
+                $header = $('header');
+            var scrollTop = $(window).scrollTop(),
+                navOffset = $header.height() - $nav.height();
+            if (scrollTop >= navOffset) {
+                $('header,body>div').addClass("_active");
+            } else {
+                $('header,body>div').removeClass("_active");
+            }
+        });
+    })
+})()
